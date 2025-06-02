de0d2689228f39784de013d21e9de0f6d5492e23
Jay-7374 <jayaprakash99956@gmail.com>
2025-06-02 18:59:36 +0530

    feat: add App1 component with styling for layout and presentation

--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,4 +1,4 @@
-
+import "./App1.css";
 import Product from "./Product";
 import Variant from "./Variant";
 import Calc from "./Calc";
@@ -6,12 +6,12 @@ export default function App1() {
   let a = 10;
   let b = 20;
   return (
-    <>
+     <div className="App1">
       <h1>This is App1 Component</h1>
       <h2>Components and Props</h2>
       <Product name="Laptop" price={45000} />
       <Variant />
       <Calc v1={a} v2={b} />
-    </>
+    </div>
   );
 }
