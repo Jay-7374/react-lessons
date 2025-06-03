46b33da69a1fbcc9fb71e30e8a410d981f76dc2a
Jay-7374 <jayaprakash99956@gmail.com>
2025-06-03 09:50:51 +0530

    fix: correct link for App8 in Header component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import App4 from "./components/App4";
 import App5 from "./components/App5";
 import App6 from "./components/App6";
 import App7 from "./components/App7";
-import App8 from "./components/App9";
+import App8 from "./components/App8";
 import App9 from "./components/App9";
 import App10 from "./components/App10";
 import App11 from "./components/App11";
